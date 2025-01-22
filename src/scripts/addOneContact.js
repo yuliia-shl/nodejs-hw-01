d4c92da59f113cab133e7f01ffc7d7cea34315d6
Yuli <yu.expert@gmail.com>
2025-01-22 19:04:51 +0200

    Step 4-5: addOneContact and getAll

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,11 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  const currentContacts = await readContacts();
+  const newContact = createFakeContact();
+  await writeContacts([...currentContacts, newContact]);
+};
 
 addOneContact();
